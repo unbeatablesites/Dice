43102d6468b31bd5dfd4873cb73254f1eae78aee
Frank Uzoka <Frankuzoka@hotmail.com>
2020-03-05 18:50:27 -0500

    Adding some logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class App {
 
     public static void main(String[] args) {
@@ -8,6 +10,16 @@ public class App {
 
     public static void  doLogic(){
         System.out.println("HERE COMES THE DICE!");
+        Random rand = new Random();
+
+        int firstNumber = 1 + rand.nextInt(6);
+        int secondNumber = 1 + rand.nextInt(6);
+
+        System.out.println("Roll #1: " + firstNumber);
+        System.out.println("Roll #2: " + secondNumber);
+
+        System.out.println("The total is " + (firstNumber + secondNumber)+"!");
+
     }
 
 
